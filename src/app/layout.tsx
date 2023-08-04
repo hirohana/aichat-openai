e891aa224cc1b4c8b6fe3377465a7fb36617f0a6
hirohana <hiro-hanatubure@outlook.jp>
2023-08-04 04:58:39 +0000

    VercelのDATABASEのパスワードによるエラーが考えられるので、再入力した。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Layout from "src/components/layouts/rootLayout/RootLayout";
 
 export const metadata = {
   title: "AIChat-OpenAI-by-Hirohana",
-  description: "create by hirohana",
+  description: "create by Hirohana",
 };
 
 export default function RootLayout({
