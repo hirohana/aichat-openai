46aa3ef0f59532e5fadd281f9cfaa0aa23f909c2
hiro <hirohanahaba@gmail.com>
2023-08-06 00:00:41 +0000

    first commi

--- src/components/elements/chatView/chatForm/chatMessageArea/ChatMessageArea.tsx
+++ src/components/elements/chatView/chatForm/chatMessageArea/ChatMessageArea.tsx
@@ -1,14 +1,27 @@
+import { KeyboardEvent, KeyboardEventHandler } from "react";
+
 type Props = {
   message: string;
   setMessage: React.Dispatch<React.SetStateAction<string>>;
+  onKeyDown: any;
 };
 
 const ChatMessageArea = (props: Props) => {
-  const { message, setMessage } = props;
+  const { message, setMessage, onKeyDown } = props;
+
+  const handleEnterKeyPress = (
+    e: KeyboardEvent<HTMLTextAreaElement>,
+    onKeyDown: KeyboardEventHandler<HTMLTextAreaElement>
+  ) => {
+    if (e.key !== "Enter") return;
+    onKeyDown(e);
+  };
+
   return (
     <textarea
       value={message}
       onChange={(e) => setMessage(e.target.value)}
+      onKeyDown={(e) => handleEnterKeyPress(e, onKeyDown)}
       placeholder="Send a message"
       className="w-full sm:max-w-5xl py-2 px-4 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent"
       rows={1}
